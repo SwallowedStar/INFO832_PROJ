24acdccab1213778a9d43a036676586c905f673f
Valentin <valentin.richard39@sfr.fr>
2023-03-08 11:15:08 +0100

    premier tests sur DiscreteActionOnOffDependent, doc non faite, tests à verifier

---
+++ src/test/java/polytech/annecy/groupeValentin/action/DiscreteActionOnOffDependentTest.java
@@ -0,0 +1,65 @@
+package polytech.annecy.groupeValentin.action;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import polytech.annecy.groupeValentin.timer.DateTimer;
+import polytech.annecy.groupeValentin.timer.Timer;
+
+import java.util.NoSuchElementException;
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiscreteActionOnOffDependentTest {
+	
+	private DiscreteAction da1;
+	private DiscreteAction da2;
+	
+	
+	@BeforeEach
+    protected void setUp() throws Exception {
+        String testString = "Valentin";
+
+        Vector<Integer> v1 = new Vector<>();
+        v1.add(5);
+        Vector<Integer> v2 = new Vector<>();
+        v2.add(4);
+
+        Timer t1 = new DateTimer(v1);
+        Timer t2 = new DateTimer(v2);
+
+        this.da1 = new DiscreteAction(testString, "toUpperCase", t1);
+        this.da2 = new DiscreteAction(testString, "toLowerCase", t2);
+    }
+
+	
+	@Test
+    public void testSpendTime() {
+        this.da1.spendTime(1);
+        this.da1.next();
+        this.da2.next();
+
+        this.da1.spendTime(1);
+        this.da2.spendTime(1);
+
+        assertEquals(4, this.da1.getCurrentLapsTime());
+        assertEquals(3, this.da2.getCurrentLapsTime());
+    }
+	
+	@Test
+    public void testNext() {
+        DiscreteAction da3 = (DiscreteAction) da1.next();
+        assertEquals(0, da3.compareTo(da1));
+        assertFalse(da3.hasNext());
+        assertThrowsExactly(NoSuchElementException.class, () -> {da3.next();});
+        assertEquals(-1, da3.compareTo(da2));
+    }
+	
+	@Test
+	public void testHasNext() {
+		assertTrue(da1.hasNext());
+	}
+
+	
+	
+}
