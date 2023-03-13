32da4f90a490c4bdcad5bdc240afba05570e8fc1
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2023-03-13 15:14:25 +0100

    Implemented the tests for the DateTimer class

---
+++ src/test/java/polytech/annecy/groupeValentin/timer/DateTimerTest.java
@@ -0,0 +1,54 @@
+package polytech.annecy.groupeValentin.timer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import polytech.annecy.groupeValentin.timer.DateTimer;
+import polytech.annecy.groupeValentin.timer.Timer;
+
+import java.util.NoSuchElementException;
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DateTimerTest {
+    private Timer t1;
+    private Timer t2;
+    
+    @BeforeEach
+    protected void setUp() throws Exception {
+        Vector<Integer> v1 = new Vector<>();
+        v1.add(5);
+        Vector<Integer> v2 = new Vector<>();
+        v2.add(4);
+        v2.add(5);
+        v2.add(1);
+        v2.add(3);
+        v2.add(-9);
+
+        this.t1 = new DateTimer(v1);
+        this.t2 = new DateTimer(v2);
+    }
+
+    @Test
+    public void testHasNext(){
+        assertTrue(this.t1.hasNext());
+        assertTrue(this.t2.hasNext());
+
+        this.t1.next();
+        this.t2.next();
+
+        assertFalse(this.t1.hasNext());
+        assertTrue(this.t2.hasNext());
+    }
+
+    @Test
+    public void testNext() {
+        assertEquals(5, this.t1.next());
+
+        assertEquals(4, this.t2.next());
+        assertEquals(5, this.t2.next());
+        assertEquals(1, this.t2.next());
+        assertEquals(3, this.t2.next());
+        assertEquals(-9, this.t2.next());
+    }
+}
