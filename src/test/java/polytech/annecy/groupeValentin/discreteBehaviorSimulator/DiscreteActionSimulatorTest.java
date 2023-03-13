2c9f3c95b4b9a581258a3bec396d94c9a6584ae6
etienne.malacarne@gmail.com <etienne.malacarne@gmail.com>
2023-03-13 17:34:28 +0100

    add test clock

--- src/test/java/polytech/annecy/groupeValentin/discreteBehaviorSimulator/DiscreteActionSimulatorTest.java
+++ src/test/java/polytech/annecy/groupeValentin/discreteBehaviorSimulator/DiscreteActionSimulatorTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DiscreteActionSimulatorTest {
 
+
+
     @Test
     void setNbLoop() {
     }
@@ -20,10 +22,20 @@ class DiscreteActionSimulatorTest {
 
     @Test
     void start() {
+        DiscreteActionSimulator ds= new DiscreteActionSimulator();
+        boolean bl=true;
+        ds.start();
+        assertEquals(bl,ds.getRunning());
+
     }
 
     @Test
     void stop() {
+
+        DiscreteActionSimulator ds= new DiscreteActionSimulator();
+        boolean bl=false;
+        ds.stop();
+        assertEquals(bl,ds.getRunning());
     }
 
     @Test
@@ -32,5 +44,9 @@ class DiscreteActionSimulatorTest {
 
     @Test
     void getRunning() {
+        DiscreteActionSimulator ds= new DiscreteActionSimulator();
+        boolean bl=false;
+
+        assertEquals(bl,ds.getRunning());
     }
 }
