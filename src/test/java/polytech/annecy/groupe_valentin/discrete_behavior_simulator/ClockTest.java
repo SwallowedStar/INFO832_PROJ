554308b88f20c4ad9db22a474311e7cf885be9b1
devinou971 <damiendasseux@gmail.com>
2023-03-14 14:48:43 +0100

    [CODE SMELL] ClockTest

--- src/test/java/polytech/annecy/groupe_valentin/discrete_behavior_simulator/ClockTest.java
+++ src/test/java/polytech/annecy/groupe_valentin/discrete_behavior_simulator/ClockTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,33 +22,22 @@ class ClockTest {
         Clock clock2 = Clock.getInstance();
 
         assertSame(clock1, clock2);
-
-    }
-
-    @Test
-    void addObserver() {
-
-    }
-
-    @Test
-    void removeObserver() {
     }
 
     @Test
     void setVirtual() {
         Clock clk=Clock.getInstance();
         boolean bl1=clk.isVirtual();
-        assertEquals(true,bl1);
+        assertTrue(bl1);
         clk.setVirtual(false);
         boolean bl2=clk.isVirtual();
-        assertEquals(false,bl2);
+        assertFalse(bl2);
     }
 
     @Test
     void isVirtual() {
-        Clock ck=Clock.getInstance();
-        boolean bool=true;
-        assertEquals(bool,ck.isVirtual());
+        Clock ck = Clock.getInstance();
+        assertTrue(ck.isVirtual());
     }
 
     @Test
@@ -83,29 +70,9 @@ class ClockTest {
 
         clk.setVirtual(false);
 
-        long dt2= clk.getTime();
-        long t_actual=new Date().getTime();
+        long dt2 = clk.getTime();
+        long tActual = new Date().getTime();
 
-        assertEquals(t_actual,dt2);
-    }
-
-    @Test
-    void lockReadAccess() {
-    }
-
-    @Test
-    void unlockReadAccess() {
-    }
-
-    @Test
-    void lockWriteAccess() {
-    }
-
-    @Test
-    void unlockWriteAccess() {
-    }
-
-    @Test
-    void testToString() {
+        assertEquals(tActual, dt2);
     }
 }
